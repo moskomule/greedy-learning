27133d6d0abef65e6ef2e69447e45bb60eec65c8
moskomule <hataya@keio.jp>
2019-02-02 08:34:33 +0900

    fix

--- greedy_learning/modules.py
+++ greedy_learning/modules.py
@@ -1,3 +1,4 @@
+from collections import deque
 from typing import Optional
 
 import torch
@@ -5,7 +6,7 @@ from homura.utils.containers import Map
 from torch import nn
 
 
-class NaiveGreedyModule(nn.Module):
+class GreedyModuleBase(nn.Module):
     def __init__(self, module: nn.Module, aux: nn.Module, tail: nn.Module):
         """
         naive implementation of greedy-learning
@@ -13,7 +14,7 @@ class NaiveGreedyModule(nn.Module):
         :param aux: nn.ModuleDict.
         :param tail: nn.Module. The rest part
         """
-        super(NaiveGreedyModule, self).__init__()
+        super(GreedyModuleBase, self).__init__()
         if not (isinstance(module, nn.ModuleDict) or isinstance(aux, nn.ModuleDict)):
             raise RuntimeError
         if set(module.keys()) <= set(aux.keys()):
@@ -23,6 +24,29 @@ class NaiveGreedyModule(nn.Module):
         self.tail = tail
         self.keys = list(self.module.keys())
         self.keys.append(None)
+        self.initialize()
+
+    def forward(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def initialize(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+
+class NaiveGreedyModule(GreedyModuleBase):
+    def __init__(self, module: nn.Module, aux: nn.Module, tail: nn.Module):
+        """
+        naive implementation of greedy-learning
+        :param module: nn.ModuleDict. module can be expressed as nn.Sequential
+        :param aux: nn.ModuleDict.
+        :param tail: nn.Module. The rest part
+        """
+        super(NaiveGreedyModule, self).__init__(module, aux, tail)
 
     def forward(self, input: torch.Tensor or Map, key: Optional[str] = None) -> torch.Tensor or Map:
         if self.training:
@@ -57,28 +81,17 @@ class NaiveGreedyModule(nn.Module):
         return nn.Sequential(*base)
 
 
-if __name__ == '__main__':
-    from homura.vision.models.cifar import resnet20
-    from utils import module_converter, Flatten
-    from torch.nn import functional as F
+class ReplayBufferGreedyModule(GreedyModuleBase):
+    def __init__(self, module: nn.Module, aux: nn.Module, tail: nn.Module,
+                 buffer_size: int = 50):
+        super(ReplayBufferGreedyModule, self).__init__(module, aux, tail)
+        self.replay_buffer = {k: deque(maxlen=buffer_size) for k in self.keys}
 
-    resnet = module_converter(resnet20(num_classes=10),
-                              keys=["conv1", "bn1", "relu", "layer1", "layer2", "layer3"])
-    aux = nn.ModuleDict({
-        "conv1": nn.Sequential(nn.AdaptiveAvgPool2d(8), nn.Conv2d(16, 16, 1, bias=False), nn.AdaptiveAvgPool2d(1),
-                               Flatten(), nn.Linear(16, 10)),
-        "layer1": nn.Sequential(nn.AdaptiveAvgPool2d(4), nn.Conv2d(16, 16, 1, bias=False), nn.AdaptiveAvgPool2d(1),
-                                Flatten(), nn.Linear(16, 10)),
-        "layer2": nn.Sequential(nn.AdaptiveAvgPool2d(2), nn.Conv2d(32, 16, 1, bias=False), nn.AdaptiveAvgPool2d(1),
-                                Flatten(), nn.Linear(16, 10)),
-        "layer3": nn.Sequential(nn.AdaptiveAvgPool2d(2), nn.Conv2d(64, 16, 1, bias=False), nn.AdaptiveAvgPool2d(1),
-                                Flatten(), nn.Linear(16, 10)),
-    })
-    greedy = NaiveGreedyModule(resnet, aux=aux,
-                               tail=nn.Sequential(nn.AdaptiveAvgPool2d(1), Flatten(), nn.Linear(64, 10)))
-    conv1_output = greedy(torch.randn(1, 3, 32, 32), "conv1")
-    F.cross_entropy(conv1_output.pred, torch.tensor([1])).backward()
-    layer1_output = greedy(conv1_output, "layer1")
-    F.cross_entropy(layer1_output.pred, torch.tensor([1])).backward()
-    final_output = greedy(layer1_output)
-    F.cross_entropy(final_output.pred, torch.tensor([1])).backward()
+    def forward(self, input: torch.Tensor or Map, straight: bool = True):
+        if not self.training or straight:
+            x = input
+            for module in self.module.values():
+                x = module(x)
+            return self.tail(x)
+        else:
+            pass

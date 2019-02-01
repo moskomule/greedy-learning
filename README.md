f226e7d6532ec7320fc0d8877280b6f736006042
moskomule <hataya@keio.jp>
2019-02-01 20:41:52 +0900

    add a simple baseline

--- README.md
+++ README.md
@@ -1,42 +1,53 @@
 # greedy-learning [WIP]
 
-To catch up recent progress on greedy (layer-wise) learning
+To catch up recent progress on greedy (layer-wise) learning.
 
-```python
-from greedy_learning.modules import NaiveGreedyModule
-from greedy_learning.utils import module_converter, Flatten
-from homura.vision.models.cifar import resnet20
-from torch import nn
+See [basic.py](greedy_learning/basic.py).
 
-# convert module to ModuleDict
-resnet = module_converter(resnet20(num_classes=10),
-                          keys=["conv1", "bn1", "relu" "layer1", "layer2", "layer3"],
-                          )
-aux = nn.ModuleDict({
-    "conv1": nn.Sequential(nn.AdaptiveAvgPool2d(2), Flatten(), nn.Linear(4*16, 10)),
-    "layer1": nn.Sequential(nn.AdaptiveAvgPool2d(2), Flatten(), nn.Linear(4*16, 10)),
-    "layer2": nn.Sequential(nn.AdaptiveAvgPool2d(2), Flatten(), nn.Linear(4*32, 10)),
-    "layer3": nn.Sequential(nn.AdaptiveAvgPool2d(2), Flatten(), nn.Linear(4*64, 10)),
-})
-# aux should have the same keys with modules
-greedy = NaiveGreedyModule(resnet, aux=aux, tail=nn.Sequential(nn.AdaptiveAvgPool2d(1), Flatten(), nn.Linear(64, 10)))
-greedy.train()
 
-# training phase
-# update 1st module
-module1_output = greedy(input, "conv1")
-F.cross_entropy(module1_output.pred, target).backward()
-
-# update 2nd module
-module2_output = greedy(module1_output, "layer1")
-F.cross_entropy(module2_output.pred, target).backward()
-...
-# update the tail
-output = greedy(module3_output)
-F.cross_entropy(output, target).backward()
-```
 
 ## requirements
 
 * Python 3.7
 * homura `pip install -U git+https://github.com/moskomule/homura`
+
+
+---
+
+## overview
+
+```python
+import torch
+from torch import nn
+from greedy_learning.modules import NaiveGreedyModule
+from greedy_learning.utils import module_converter, Flatten
+from homura.vision.models.cifar import resnet20
+
+from homura.vision.models.cifar import resnet20
+from utils import module_converter, Flatten
+from torch.nn import functional as F
+
+# base model need to be simple, i.e. can be expressed as nn.Sequential style
+# then convert model to nn.ModuleDict
+resnet = module_converter(resnet20(num_classes=10),
+                          keys=["conv1", "bn1", "relu", "layer1", "layer2", "layer3"])
+aux = nn.ModuleDict({
+    "conv1": nn.Sequential(nn.AdaptiveAvgPool2d(8), nn.Conv2d(16, 16, 1, bias=False), nn.AdaptiveAvgPool2d(1),
+                           Flatten(), nn.Linear(16, 10)),
+    "layer1": nn.Sequential(nn.AdaptiveAvgPool2d(4), nn.Conv2d(16, 16, 1, bias=False), nn.AdaptiveAvgPool2d(1),
+                            Flatten(), nn.Linear(16, 10)),
+    "layer2": nn.Sequential(nn.AdaptiveAvgPool2d(2), nn.Conv2d(32, 16, 1, bias=False), nn.AdaptiveAvgPool2d(1),
+                            Flatten(), nn.Linear(16, 10)),
+    "layer3": nn.Sequential(nn.AdaptiveAvgPool2d(2), nn.Conv2d(64, 16, 1, bias=False), nn.AdaptiveAvgPool2d(1),
+                            Flatten(), nn.Linear(16, 10)),
+})
+greedy = NaiveGreedyModule(resnet, aux=aux,
+                           tail=nn.Sequential(nn.AdaptiveAvgPool2d(1), Flatten(), nn.Linear(64, 10)))
+conv1_output = greedy(torch.randn(1, 3, 32, 32), "conv1")
+F.cross_entropy(conv1_output.pred, torch.tensor([1])).backward()
+layer1_output = greedy(conv1_output, "layer1")
+F.cross_entropy(layer1_output.pred, torch.tensor([1])).backward()
+# to update the tail
+final_output = greedy(layer1_output)
+F.cross_entropy(final_output.pred, torch.tensor([1])).backward()
+```
---
+++ greedy_learning/basic.py
@@ -0,0 +1,90 @@
+from homura import optim
+from homura.utils import trainer as _trainer, callbacks as _callbacks, reporter
+from homura.utils.containers import Map
+from homura.vision.data import cifar10_loaders
+from homura.vision.models.cifar import resnet20
+from torch import nn
+
+from modules import NaiveGreedyModule
+from utils import module_converter, Flatten
+
+
+class Trainer(_trainer.TrainerBase):
+    def __init__(self, model, optimizer, loss_f, callbacks):
+        super(Trainer, self).__init__(model, optimizer, loss_f, callbacks=callbacks)
+        self.keys = list(self.model.aux.keys())
+        self.keys.append(None)
+
+    def iteration(self, data):
+        input, target = data
+        if self.is_train:
+            output_map = self.train_iteration(input, target)
+        else:
+            output = self.model(input)
+            loss = self.loss_f(output, target)
+            output_map = Map(output=output, loss=loss,
+                             # todo: fix homura
+                             loss_conv1=0, loss_layer1=0, loss_layer2=0, loss_layer3=0)
+        return output_map
+
+    def train_iteration(self, input, target):
+        output_map = Map()
+        for key in self.keys:
+            input = self.model(input, key)
+            loss = self.loss_f(input.pred, target)
+            output_map[f"loss_{key}"] = loss
+
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+            input.features.detach_()
+
+        output_map.output = input.pred  # last one
+        output_map.loss = output_map.pop(f"loss_{None}")
+        return output_map
+
+
+def greedy_loss_by_name(name):
+    name = f"loss_{name}"
+
+    @_callbacks.metric_callback_decorator(name=name)
+    def f(data):
+        return data[name]
+
+    return f
+
+
+if __name__ == '__main__':
+    import miniargs
+    from torch.nn import functional as F
+
+    p = miniargs.ArgumentParser()
+    p.add_int("--batch_size", default=128)
+    args = p.parse()
+
+    keys = ["conv1", "bn1", "relu", "layer1", "layer2", "layer3"]
+
+    train_loader, test_loader = cifar10_loaders(args.batch_size)
+    resnet = module_converter(resnet20(num_classes=10), keys=keys)
+    aux = nn.ModuleDict({
+        "conv1": nn.Sequential(nn.AdaptiveAvgPool2d(8), nn.Conv2d(16, 16, 1, bias=False), nn.AdaptiveAvgPool2d(1),
+                               Flatten(), nn.Linear(16, 10)),
+        "layer1": nn.Sequential(nn.AdaptiveAvgPool2d(4), nn.Conv2d(16, 16, 1, bias=False), nn.AdaptiveAvgPool2d(1),
+                                Flatten(), nn.Linear(16, 10)),
+        "layer2": nn.Sequential(nn.AdaptiveAvgPool2d(2), nn.Conv2d(32, 16, 1, bias=False), nn.AdaptiveAvgPool2d(1),
+                                Flatten(), nn.Linear(16, 10)),
+        "layer3": nn.Sequential(nn.AdaptiveAvgPool2d(2), nn.Conv2d(64, 16, 1, bias=False), nn.AdaptiveAvgPool2d(1),
+                                Flatten(), nn.Linear(16, 10)),
+    })
+    greedy = NaiveGreedyModule(resnet, aux=aux,
+                               tail=nn.Sequential(nn.AdaptiveAvgPool2d(1), Flatten(), nn.Linear(64, 10)))
+    tb = reporter.TensorboardReporter([_callbacks.LossCallback(), _callbacks.AccuracyCallback(),
+                                       greedy_loss_by_name("conv1"),
+                                       greedy_loss_by_name("layer1"),
+                                       greedy_loss_by_name("layer2"),
+                                       greedy_loss_by_name("layer3")],
+                                      "../results")
+    trainer = Trainer(greedy, optim.Adam(lr=3e-4, weight_decay=1e-4), F.cross_entropy, callbacks=tb)
+    for _ in range(50):
+        trainer.train(train_loader)
+        trainer.test(test_loader)
